0a74861d51f3152fea4d3bd4a1b560428073ab49
hazennetworksolutions <161881320+hazennetworksolutions@users.noreply.github.com>
2024-11-25 21:49:55 +0300

    Create 0.4.0-autoscript.sh

---
+++ 0.4.0-autoscript.sh
@@ -0,0 +1,247 @@
+#!/bin/bash
+LOG_FILE="/var/log/0g_node_install.log"
+exec > >(tee -a "$LOG_FILE") 2>&1
+
+printGreen() {
+    echo -e "\033[32m$1\033[0m"
+}
+
+printLine() {
+    echo "------------------------------"
+}
+
+# Function to print the node logo
+function printNodeLogo {
+    echo -e "\033[32m"
+    echo "          
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+██████████████████████████████████████████████        ██████████████████████████████████████████████
+███████████████████████████████████████████              ███████████████████████████████████████████
+████████████████████████████████████████                    ████████████████████████████████████████
+█████████████████████████████████████                          █████████████████████████████████████
+█████████████████████████████████                                  █████████████████████████████████
+██████████████████████████████             █             █            ██████████████████████████████
+████████████████████████████           █████             ████           ████████████████████████████
+████████████████████████████          ██████             ██████         ████████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ███████            ██████          ███████████████████████████
+████████████████████████████          ██████████         ██████          ███████████████████████████
+████████████████████████████          █████████████      ██████          ███████████████████████████
+████████████████████████████             █████████████     ████          ███████████████████████████
+████████████████████████████          █     █████████████     █          ███████████████████████████
+████████████████████████████          █████     ████████████             ███████████████████████████
+████████████████████████████          ██████       ████████████          ███████████████████████████
+████████████████████████████          ██████          █████████          ███████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ██████             ██████         ████████████████████████████
+████████████████████████████            ████             ███            ████████████████████████████
+██████████████████████████████                                        ██████████████████████████████
+█████████████████████████████████                                  █████████████████████████████████
+█████████████████████████████████████                           ████████████████████████████████████
+████████████████████████████████████████                    ████████████████████████████████████████
+███████████████████████████████████████████              ███████████████████████████████████████████
+██████████████████████████████████████████████        ██████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+Hazen Network Solutions 2024 All rights reserved."
+    echo -e "\033[0m"
+}
+
+# Show the node logo
+printNodeLogo
+
+# User confirmation to proceed
+echo -n "Type 'yes' to start the installation 0G Labs v0.4.0 and press Enter: "
+read user_input
+
+if [[ "$user_input" != "yes" ]]; then
+  echo "Installation cancelled."
+  exit 1
+fi
+
+# Function to print in green
+printGreen() {
+  echo -e "\033[32m$1\033[0m"
+}
+
+printGreen "Starting installation..."
+sleep 1
+
+printGreen "If there are any, clean up the previous installation files"
+
+sudo systemctl stop 0gchaind
+sudo systemctl disable 0gchaind
+sudo rm -rf /etc/systemd/system/0gchaind.service
+sudo rm $(which 0gchaind)
+sudo rm -rf $HOME/.0gchain
+sed -i "/0gchaind_/d" $HOME/.bash_profile
+
+# Update packages and install dependencies
+printGreen "1. Updating and installing dependencies..."
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
+
+# User inputs
+read -p "Enter your MONIKER: " MONIKER
+echo 'export MONIKER='$MONIKER
+read -p "Enter your PORT (2-digit): " PORT
+echo 'export PORT='$PORT
+
+# Setting environment variables
+echo "export MONIKER=$MONIKER" >> $HOME/.bash_profile
+echo "export OG_CHAIN_ID=\"zgtendermint_16600-2\"" >> $HOME/.bash_profile
+echo "export OG_PORT=$PORT" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+printLine
+echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
+echo -e "Chain ID:       \e[1m\e[32m$OG_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$OG_PORT\e[0m"
+printLine
+sleep 1
+
+# Install Go
+printGreen "2. Installing Go..." && sleep 1
+cd $HOME
+VER="1.23.0"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=\$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+# Version check
+echo $(go version) && sleep 1
+
+# Download Prysm protocol binary
+printGreen "3. Downloading 0G Labs binary and setting up..." && sleep 1
+cd $HOME
+wget -O 0gchaind https://github.com/0glabs/0g-chain/releases/download/v0.4.0/0gchaind-linux-v0.4.0
+chmod +x $HOME/0gchaind
+mkdir -p $HOME/.0gchain/cosmovisor/genesis/bin
+mv /root/0gchaind $HOME/.0gchain/cosmovisor/genesis/bin/0gchaind
+sudo ln -s $HOME/.0gchain/cosmovisor/genesis $HOME/.0gchain/cosmovisor/current -f
+sudo ln -s $HOME/.0gchain/cosmovisor/current/bin/0gchaind /usr/local/bin/0gchaind -f
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.6.0
+
+# Create service file
+printGreen "6. Creating service file..." && sleep 1
+sudo tee /etc/systemd/system/0gchaind.service > /dev/null << EOF
+[Unit]
+Description=0gchaind node service
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start --json-rpc.api eth,txpool,personal,net,debug,web3
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/.0gchain"
+Environment="DAEMON_NAME=0gchaind"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.0gchain/cosmovisor/current/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+# Enable the service
+sudo systemctl daemon-reload
+sudo systemctl enable 0gchaind
+
+# Initialize the node
+printGreen "7. Initializing the node..."
+0gchaind config set client chain-id ${PRYSM_CHAIN_ID}
+0gchaind config set client keyring-backend test
+0gchaind config set client node tcp://localhost:${PRYSM_PORT}657
+0gchaind init ${MONIKER} --chain-id ${PRYSM_CHAIN_ID}
+
+# Download genesis and addrbook files
+printGreen "8. Downloading genesis and addrbook..."
+wget -O $HOME/.0gchain/config/genesis.json https://raw.githubusercontent.com/hazennetworksolutions/0glabs/refs/heads/main/genesis.json
+wget -O $HOME/.0gchain/config/addrbook.json  https://raw.githubusercontent.com/hazennetworksolutions/0glabs/refs/heads/main/addrbook.json
+
+# Configure gas prices and ports
+printGreen "9. Configuring custom ports and gas prices..." && sleep 1
+sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "0ua0gi"|g' $HOME/.0gchain/config/app.toml
+sed -i.bak -e "s%:1317%:${OG_PORT}317%g;
+s%:8080%:${OG_PORT}080%g;
+s%:9090%:${OG_PORT}090%g;
+s%:9091%:${OG_PORT}091%g;
+s%:8545%:${OG_PORT}545%g;
+s%:8546%:${OG_PORT}546%g;
+s%:6065%:${OG_PORT}065%g" $HOME/.0gchain/config/app.toml
+
+# Configure P2P and ports
+sed -i.bak -e "s%:26658%:${OG_PORT}658%g;
+s%:26657%:${OG_PORT}657%g;
+s%:6060%:${OG_PORT}060%g;
+s%:26656%:${OG_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${OG_PORT}656\"%;
+s%:26660%:${OG_PORT}660%g" $HOME/.0gchain/config/config.toml
+
+# Set up seeds and peers
+printGreen "10. Setting up peers and seeds..." && sleep 1
+SEEDS="8f21742ea5487da6e0697ba7d7b36961d3599567@og-testnet-seed.itrocket.net:47656"
+PEERS="80fa309afab4a35323018ac70a40a446d3ae9caf@og-testnet-peer.itrocket.net:11656,4e7e6e9a3bc116612644d11b43c9b32b4003bb2c@37.27.128.102:26656,e5c21825ac19a9056046f4dd7abbde7915b17a68@142.132.213.214:12656,1e0d82b9b12401376d595bafd0652b95918e50a9@161.97.137.134:26656,88a20c8881c36c0d530ccb22e148f620941142e9@109.199.98.178:47656,219e0d480e699ba0d361049c0ce5a020f443d5f8@45.159.229.204:12656,feec86b33e5d46c83501e4c3ddb6471653f158db@194.163.181.101:47656,9b75c700c6f27a98238bb9f91a5a351e671dc46a@65.21.210.235:26656,a8bd58cea1dc17b5f8e301d489db0239d05111a8@86.48.7.81:12656,c0dab875b2e19d74a830b4a13393b004d8bf9504@84.21.171.218:12656,1428997a0b1e534494c497afcd18eaca40734955@194.163.179.111:12656,dcf6ccb1c3d7f3ee21cc3c91c75bff131c2f6a5e@185.192.97.246:26656,802e9a34b11dc5de9c2266bcd9413e076b2ff146@185.190.140.223:26656,4109414b09167ad2dde0a0265827497187d906a3@89.117.58.218:12656,9c665db23dbbe8a667910fb5e1482908a27ed69e@45.159.229.205:12656,16c6c589b8fcebb5b451282f89d93230973881eb@77.237.239.54:12656,922e74b2a646b0549979596bdfbeda426c3adeac@158.220.99.241:12656,aa382c9d4710b2f45cf081a75b8237dfdf0ff105@38.242.158.60:12656,71e01e28fdf9c09dbd5229ecdf3d97c584c89385@149.50.96.112:26656,66f984cc027e0ed92a0e6a78fe2e39afbc77afe9@212.90.120.12:12656,2daf5337b37cd528df76089ebeb86204e64bb9cf@38.242.247.180:12656"
+sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
+       -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" $HOME/.0gchain/config/config.toml
+
+# Pruning Settings
+printGreen "12. Setting up pruning config..." && sleep 1
+sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "0ua0gi"|g' $HOME/.0gchain/config/app.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.0gchain/config/config.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.0gchain/config/config.toml
+
+# Download the snapshot
+# printGreen "12. Downloading snapshot and starting node..." && sleep 1
+
+
+
+
+
+# Start the node
+printGreen "13. Starting the node..."
+sudo systemctl start 0gchaind
+
+# Check node status
+printGreen "14. Checking node status..."
+sudo journalctl -u 0gchaind -f -o cat
+
+# Verify if the node is running
+if systemctl is-active --quiet 0gchaind; then
+  echo "The node is running successfully! Logs can be found at /var/log/0g_node_install.log"
+else
+  echo "The node failed to start. Logs can be found at /var/log/0g_node_install.log"
+fi
